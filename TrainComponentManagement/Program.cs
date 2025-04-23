25f2f6263ce8c59f07d423a4d0685c7d17376ffd
Юрій Куник <kunyk.yuri@gmail.com>
2025-04-23 20:56:59 +0300

    feat: Initialize ASP.NET Core project files

---
+++ TrainComponentManagement/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using TrainComponentManagement.Data;
+using TrainComponentManagement.Services;
+
+namespace TrainComponentManagement;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddControllers();
+        
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.MapControllers();
+
+        app.Run();
+
+    }
+}
