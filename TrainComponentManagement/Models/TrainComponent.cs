1d9eee3fcc11d2dcec61706c9afb0dc70e8d3eeb
Юрій Куник <kunyk.yuri@gmail.com>
2025-04-23 20:59:21 +0300

    feat: define TrainComponent entity

---
+++ TrainComponentManagement/Models/TrainComponent.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrainComponentManagement.Entities;
+
+public class TrainComponent
+{
+    public int Id { get; set; } // Primary Key
+    [Required]
+    [MaxLength(100)]
+    public  string Name { get; set; }
+    [Required]
+    [MaxLength(50)]
+    public  string UniqueNumber { get; set; }
+    public bool CanAssignQuantity { get; set; }
+    public int? Quantity { get; set; } // Nullable int
+}
